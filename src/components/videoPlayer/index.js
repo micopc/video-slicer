522f3f9aef92907975a34ee759f3ed1f4f1dd7a6
Gabriel Pena Calero <gabrielpenac25@gmail.com>
2018-12-15 14:49:35 -0500

    Add storage capabilities.

--- src/components/videoPlayer/index.js
+++ src/components/videoPlayer/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 import videoAsset from '../../assets/video.mp4'
-import { Container, Video } from './style'
+import { Container, VideoContainer, Video, LoadingOverlay } from './style'
 import Playlist from './components/playlist'
 
 const ORIGINAL_VIDEO = {
@@ -11,19 +13,50 @@ const ORIGINAL_VIDEO = {
 
 class VideoPlayer extends React.Component {
   state = {
-    clips: [],
+    clips: this.loadClips(),
     activeClip: ORIGINAL_VIDEO,
-    videoDuration: 0
+    videoDuration: 0,
+    loading: false,
+    shouldAutoPlay: false
+  }
+
+  videoEndReached = false
+
+  componentDidMount() {
+    this.setAutoPlay()
+  }
+
+  setAutoPlay = () => {
+    this.setState({ shouldAutoPlay: true })
+  }
+
+  loadClips() {
+    const storedClips = localStorage.getItem('clips')
+    if (!storedClips) {
+      return []
+    }
+
+    return JSON.parse(storedClips)
+  }
+
+  saveClips(clips) {
+    localStorage.setItem('clips', JSON.stringify(clips))
   }
 
   onAddClip = clip => {
-    this.setState({ clips: [...this.state.clips, clip] })
+    const newClips = [...this.state.clips, clip]
+
+    this.saveClips(newClips)
+
+    this.setState({ clips: newClips })
   }
 
   onRemoveClip = index => {
     const { clips, activeClip } = this.state
 
     const newClips = [...clips.slice(0, index), ...clips.slice(index + 1)]
+
+    this.saveClips(newClips)
 
     const newState = { clips: newClips }
 
@@ -39,6 +72,8 @@ class VideoPlayer extends React.Component {
     const { clips, activeClip } = this.state
 
     const newClips = [...clips.slice(0, index), clip, ...clips.slice(index + 1)]
+
+    this.saveClips(newClips)
 
     const newState = { clips: newClips }
 
@@ -59,6 +94,54 @@ class VideoPlayer extends React.Component {
     this.setState({ videoDuration: Math.round(e.target.duration) })
   }
 
+  onVideoPlay = e => {
+    console.log('play')
+    this.videoEndReached = false
+  }
+
+  // onVideoTimeUpdate runs while the clips are playing.
+  // When the end of the current clip is reached
+  // A loading overlay is shown for 3 seconds and then the next video is played
+  // It was necessary since the HTML5 video event "onEnded" only works for the whole video not fragments
+  onVideoTimeUpdate = e => {
+    console.log('end', this.videoEndReached)
+    const currentTime = Math.floor(e.target.currentTime)
+
+    if (currentTime >= this.state.activeClip.end && !this.videoEndReached) {
+      this.videoEndReached = true
+
+      this.playNextVideo()
+    }
+  }
+
+  onVideoEnded = e => {
+    this.videoEndReached = true
+
+    this.playNextVideo()
+  }
+
+  playNextVideo = () => {
+    this.setState({ loading: true }, () => {
+      setTimeout(() => {
+        const { activeClip, clips } = this.state
+
+        const activeIndex = clips.findIndex(clip => clip.id === activeClip.id)
+
+        if (activeIndex === clips.length - 1) {
+          this.setState({ loading: false, activeClip: ORIGINAL_VIDEO })
+          return
+        }
+
+        if (activeIndex === -1) {
+          this.setState({ loading: false, activeClip: clips[0] })
+          return
+        }
+
+        this.setState({ loading: false, activeClip: clips[clips.length - 1] })
+      }, 5000)
+    })
+  }
+
   getVideoSrc = () => {
     const { activeClip } = this.state
 
@@ -74,20 +157,38 @@ class VideoPlayer extends React.Component {
   }
 
   render() {
-    const { clips, activeClip, videoDuration } = this.state
+    const {
+      clips,
+      activeClip,
+      videoDuration,
+      loading,
+      shouldAutoPlay
+    } = this.state
 
     const videoSrc = this.getVideoSrc()
 
     return (
       <Container>
-        <Video
-          key={videoSrc}
-          controls
-          onDurationChange={this.onVideoDurationChange}
-        >
-          <source src={videoSrc} />
-          Sorry, your browser doesn't support embedded videos.
-        </Video>
+        <VideoContainer>
+          <Video
+            key={videoSrc}
+            controls
+            autoPlay={shouldAutoPlay}
+            onPlay={this.onVideoPlay}
+            onDurationChange={this.onVideoDurationChange}
+            onTimeUpdate={this.onVideoTimeUpdate}
+            onEnded={this.onVideoEnded}
+          >
+            <source src={videoSrc} />
+            Sorry, your browser doesn't support embedded videos.
+          </Video>
+          {loading && (
+            <LoadingOverlay>
+              <FontAwesomeIcon icon={faSpinner} color="#fff" pulse size="3x" />
+              <p>The next clip will start soon</p>
+            </LoadingOverlay>
+          )}
+        </VideoContainer>
         <Playlist
           originalVideo={ORIGINAL_VIDEO}
           videoDuration={videoDuration}
--- src/components/videoPlayer/style.js
+++ src/components/videoPlayer/style.js
@@ -6,7 +6,27 @@ export const Container = styled.div`
   width: 100%;
 `
 
+export const VideoContainer = styled.div`
+  position: relative;
+`
+
 export const Video = styled.video`
   width: 100%;
   height: auto;
+`
+
+export const LoadingOverlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #000;
+  opacity: 0.8;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+  color: #fff;
 `
